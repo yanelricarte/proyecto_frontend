fe300a3cb6c4f72f3420f546cb19aba69024d10c
Naranjas <naranjaspintadas@gmail.com>
2022-09-27 20:42:58 -0300

    clase 10

---
+++ clase_10/js/main.js
@@ -0,0 +1,63 @@
+/** Clase 10: Intro js */
+
+/*
+console.log("hola mundo desde archivo externo");
+
+// VARIABLES
+
+
+var nomb = "Harry";
+
+console.log(nomb);
+
+// STRING
+let nombre = "Harry";
+const NOMBRE = "Harry";
+
+console.log(nombre);
+
+
+// NUMERO
+let edad = 20;
+const NUM = 10.2;
+
+console.log(edad);
+
+
+let tenerMascotas = true; //Booleana
+
+
+// Operaciones
+
+let numeroA = 12;
+let numeroB = 3;
+
+let resultadoSuma = numeroA + numeroB;
+console.log(resultadoSuma);
+
+let resultadoMul = numeroA * numeroB;
+console.log(resultadoMul);
+
+
+let textoA = "Hola";
+let textoB = "Mundo";
+const BLANCO = " ";
+
+
+let resultadoA = textoA + BLANCO + textoB;
+
+console.log(resultadoA);
+
+// --->PROMT
+
+let entrada = prompt ("Ingrese su nombre");
+let salida = "Hola "+entrada;
+alert(salida);
+*/
+
+
+let numero = parseInt(prompt("Ingrese un numero"));
+const numeroB = 30;
+let salida = numero + numeroB;
+alert(salida);
+
